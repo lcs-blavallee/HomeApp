b94cdbfcaa61659e992b6adee9d22a25915c8f37
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-09 20:23:02 -0500

    I added custom colours in the structure

--- HomeApp/ContentView.swift
+++ HomeApp/ContentView.swift
@@ -8,6 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let lightBlue = Color(
+            hue: 204/360.0,
+            saturation: 0.525,
+            brightness: 0.604
+        )
+    
+    let lightTurquoise = Color(
+            hue: 188/360.0,
+            saturation: 0.386,
+            brightness: 0.578
+        )
+    
+    let lightYellow = Color(
+            hue: 47/360.0,
+            saturation: 0.468,
+            brightness: 0.514
+        )
+    
+    let circleBlue = Color(
+            hue: 233/360.0,
+            saturation: 0.435,
+            brightness: 0.18
+        )
+    
+    let rectangleBlue = Color(
+            hue: 240/360.0,
+            saturation: 0.407,
+            brightness: 0.212
+        )
+    
+    let rectangleGray = Color(
+            hue: 230/360.0,
+            saturation: 0.20,
+            brightness: 0.755
+        )
+    
     var body: some View {
         NavigationStack {
             ZStack {
